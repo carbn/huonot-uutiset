62efbe9029f5de9852e1108c7d4e7b6ffef014f3
Samu Nuutamo <samu.nuutamo@gmail.com>
2015-09-16 19:06:03 +0300

    Added a basic home page template

--- huonotuutiset/views.py
+++ huonotuutiset/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Avg
 
-# Create your views here.
+from huonotuutiset.models import Site
+
+
+def home(request):
+    context = {
+        'sites': Site.objects.annotate(score=Avg('newsitems__score')).order_by('-score')
+    }
+    return render(request, 'home.html', context)
