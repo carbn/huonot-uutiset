4516468db6583f50f49853b116ef930a8581e398
Samu Nuutamo <samu.nuutamo@gmail.com>
2015-09-17 23:09:01 +0300

    Added real stats to the home view

--- huonotuutiset/views.py
+++ huonotuutiset/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Avg
+from django.db.models import Avg, Count
 
-from huonotuutiset.models import Site
+from huonotuutiset.models import Site, NewsItem
 
 
 def home(request):
     context = {
-        'sites': Site.objects.annotate(score=Avg('newsitems__score')).order_by('-score')
+        'worst_titles': NewsItem.objects.filter(score__isnull=False).order_by('-score')[:3],
+        'worst_sites_avg': Site.objects.filter(newsitems__score__isnull=False).annotate(score=Avg('newsitems__score')).order_by('-score')[:3],
+        'worst_sites_abs': Site.objects.filter(newsitems__score__gt=1.0).annotate(bad_count=Count('newsitems__score')).order_by('-bad_count')[:3],
     }
+
     return render(request, 'home.html', context)
