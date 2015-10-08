4486d205a771138135e84518fbc4e64d9b925cef
Samu Nuutamo <samu.nuutamo@gmail.com>
2015-10-08 22:56:01 +0300

    Better stats in the home view
    * Weekly scope
    * Percentages of bad titles
    * Parametrized time frame

--- huonotuutiset/urls.py
+++ huonotuutiset/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^tilastot/(?P<year>[0-9]{4})/(?P<week>[0-9]{2})/$', views.home, name='weekly_stats'),
+    url(r'^sivusto/(?P<id>[0-9]+)/$', views.site, name='site'),
     url(r'^tietoa$', views.home, name='about'),
 ]
--- huonotuutiset/views.py
+++ huonotuutiset/views.py
@@ -1,14 +1,77 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
-from django.db.models import Avg, Count
+from django.db.models import Avg, Count, F, Sum, Case, When, IntegerField
+from django.utils import timezone
+from isoweek import Week
 
 from huonotuutiset.models import Site, NewsItem
 
 
-def home(request):
+def home(request, year=None, week=None):
+    if not year or not week:
+        # use previous week by default
+        week = Week.thisweek() - 1
+    else:
+        week = Week(int(year), int(week))
+
+    date_range = get_date_range(week)
+
+    # absolute worst titles
+    worst_titles = NewsItem.objects.filter(published__range=date_range, score__isnull=False).order_by('-score')[:5]
+
+    # worst sites on average
+    site_avg = Site.objects.filter(
+            newsitems__published__range=date_range,
+            newsitems__score__isnull=False
+        ).annotate(
+            score=Avg('newsitems__score')
+        ).order_by('-score')[:5]
+
+    # biggest percentage of bad titles
+    sites = Site.objects.filter(
+            newsitems__published__range=date_range,
+            newsitems__score__isnull=False
+        ).annotate(
+            bad=Sum(Case(When(newsitems__score__gt=1.0, then=1), output_field=IntegerField())),
+            total=Count('newsitems__score'),
+        )
+
+    percentages = []
+
+    # calculate percentages
+    for site in sites:
+        bad = float(site.bad if site.bad else 0)
+        total = float(site.total if site.total else 0)
+
+        percentage = int((bad/total)*100)
+
+        percentages.append((percentage, site))
+
+    # sort by percentage and take top 5
+    percentages = list(reversed(sorted(percentages, key=lambda x: x[0])))[:5]
+
     context = {
-        'worst_titles': NewsItem.objects.filter(score__isnull=False).order_by('-score')[:3],
-        'worst_sites_avg': Site.objects.filter(newsitems__score__isnull=False).annotate(score=Avg('newsitems__score')).order_by('-score')[:3],
-        'worst_sites_abs': Site.objects.filter(newsitems__score__gt=1.0).annotate(bad_count=Count('newsitems__score')).order_by('-bad_count')[:3],
+        'worst_titles': worst_titles,
+        'site_avg': site_avg,
+        'bad_percentages': percentages,
     }
 
     return render(request, 'home.html', context)
+
+
+def site(request, id):
+    site = Site.objects.get(id=id)
+
+    context = {
+        'site': site,
+    }
+
+    return render(request, 'site.html', context)
+
+
+def get_date_range(week):
+    start = timezone.make_aware(datetime.fromordinal(week.monday().toordinal()))
+    end = start + timedelta(weeks=1)
+
+    return [start, end]
